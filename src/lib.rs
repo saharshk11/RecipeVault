e668acd18fe39d89eb90e4baa82a4e6b584fef7d
Saharsh Koganti <saharshk11@gmail.com>
2025-12-26 16:39:37 -0600

    added fallback html scraping in case json_ld is not provided

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod model;
 mod error;
 mod jsonld;
+mod html_fallback;
 
 pub use model::Recipe;
 pub use error::RecipeError;
@@ -9,6 +10,10 @@ use url::Url;
 
 pub fn extract_recipe(html: &str, base_url: &Url) -> Result<Recipe, RecipeError> {
     if let Some(recipe) = jsonld::try_extract_recipe_from_jsonld(html, base_url)? {
+        return Ok(recipe);
+    }
+
+    if let Some(recipe) = html_fallback::try_extract_recipe_from_html(html, base_url)? {
         return Ok(recipe);
     }
 
